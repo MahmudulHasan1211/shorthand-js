61ffa99b241c9f2433b157879040261f08cc7c2b
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-01 00:19:33 +0600

    first commit

---
+++ shorthand.js
@@ -0,0 +1,15 @@
+var price = 95;
+price +=5; //shorthand addition
+console.log(price)
+
+var priceO = 10000;
+priceO -= 2000; //shorthand subtraction
+console.log(priceO)
+
+var orangeP = 10000;
+orangeP *= 2000; //shorthand Multiplication
+console.log(orangeP)
+
+var orangePp = 10000;
+orangePp /= 2000; //shorthand Multiplication
+console.log(orangePp)
